85f2343be498c3b7bdb85977f1e91a42bc7b682e
stackedqueue <yang.julian@gmail.com>
2015-02-13 15:59:55 -0800

    adds a bash script useful for compiling and copying executables to the /var/www/html/ directory. Use case: ./apache_sp.sh <filename> NOTE: do NOT put the .cpp in <filename>

---
+++ blockcache/apache_cp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#echo -e "Enter .cpp file name (without the ".cpp"): \c"
+
+if [ "$#" -ne 1 ]; then
+    echo "Only 1 parameter: filename of .cpp (without .cpp)"
+    exit 1
+fi
+
+name=$1
+fcgidName="${name}.fcgid"
+
+echo -e "Compiling: g++ ${name}.cpp -o $fcgidName -lfcgi"
+`g++ "${name}.cpp" -o $fcgidName -lfcgi`
+`sudo rm "/var/www/html/${fcgidName}"`
+echo -e "...removed old copy at /var/www/html/"
+`sudo cp $fcgidName "/var/www/html/"`
+echo -e "...copying new version"
+`sudo chmod 633 "/var/www/html/${fcgidName}"`
+echo -e "...added execute and read permissions (633)"
