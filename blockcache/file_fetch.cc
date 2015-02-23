7492a3c7c7ce4f2546605a6d2d342ae8245382f1
rchen93 <rchen93@ucla.edu>
2015-02-23 00:55:19 -0800

    Updated block_api for clarity
    -Fixed minor bug in file_fetch

--- blockcache/file_fetch.cc
+++ blockcache/file_fetch.cc
@@ -154,7 +154,6 @@ int main(void) {
         string dbName = "mydb";
         leveldb::DB *db;
     	leveldb::Options options;
-    	options.create_if_missing = true;
     	leveldb::Status status = leveldb::DB::Open(options, "mydb", &db);
     	if (!status.ok()) {
         	outputErrorMessage(status.ToString());
