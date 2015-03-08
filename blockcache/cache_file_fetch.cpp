d287eba06adc7efb35d0622d2ef8f15d44f946d4
stackedqueue <yang.julian@gmail.com>
2015-03-08 15:52:26 -0700

    Refactor out sendHttpRequest to make it more generic and easier to reuse its code

--- blockcache/cache_file_fetch.cpp
+++ blockcache/cache_file_fetch.cpp
@@ -33,65 +33,23 @@ using namespace std;
 
 int const MSG_SIZE = 1000;
 
-int httpResponseReader(void *data, const char *buf, size_t len)
-{
-    string *str = (string *)data;
-    str->append(buf, len);
-    return 0;
-}
-
 //forwards the HTTP request to the actual Block Server
 //TODO: change host & query to the correct values when finalized
 int requestFromBlockServer(string hash, string block) {
     //make request here.
     string host = "127.0.0.1";
-    string query = "/file_fetch.fcgid?hash=" + hash;
-    ne_session *sess;
-    ne_request *req;
+    string path = "/file_fetch.fcgid?hash=" + hash;
     string response;
-    
-    //block = block + "test123";
+    string responseHeader;
 
-    sess = ne_session_create("http", host.c_str(), 80);
+    HttpHelper::sendHttpRequest(host, path, "GET", block, responseHeader, 
+            response);
 
-    req = ne_request_create(sess, "GET", query.c_str());
-    ne_set_request_body_buffer(req, block.c_str(), block.length());
-
-    ne_add_response_body_reader(req, ne_accept_always, httpResponseReader, &response);
-    int result = ne_request_dispatch(req);
-    int status = ne_get_status(req)->code;
-    string responseHeader(ne_get_response_header(req, "Content-Type"));
-
-    ne_request_destroy(req);
-    
     printf("Content-Type:  %s\r\n\r\n", responseHeader.c_str());
     printf("%s", response.c_str());
     
     return 0;
 }
-
-//TODO: refactor Roger's file_fetch hashBlock #'s so that this code uses it too
-//int getQueryParam(const string& query_string, const string& param, string& value) {
-//	// Verify that the parameter required is found
-//	int paramPos = query_string.find(param);
-//    
-//	if (paramPos == string::npos) {
-//		return 1;
-//	}
-//
-//	int valuePos = query_string.find("=", paramPos) + 1;
-//    int nextParam = query_string.find("&", valuePos);
-//    
-//    //if there is another parameter after the one we're searching for
-//    if (nextParam == string::npos) {
-//	    value = query_string.substr(valuePos);    
-//    } else {
-//        value = query_string.substr(valuePos, nextParam - valuePos);
-//
-//    }
-//
-//	return 0;
-//}
 
 void error(const char *msg)
 {
@@ -174,7 +132,7 @@ int main(void)
         //requested hash locally.
         
         //if(have block already) {
-        //  create resposne and insert block data into response body
+        //  create response and insert block data into response body
         //  return;
         //}
         //else {
