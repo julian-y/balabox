1a8efb44e2143276d72cddbb378ac571e5762cca
rchen93 <rchen93@ucla.edu>
2015-03-13 00:24:20 -0700

    Clean up LevelDB wrapper

--- blockcache/leveldb_helper.cc
+++ blockcache/leveldb_helper.cc
@@ -11,10 +11,11 @@ LevelDBHelper::LevelDBHelper(const string& db_name) {
 	writeoptions.sync = true;
 	leveldb::Status status = leveldb::DB::Open(options, db_name, &m_db);
 	if (!status.ok()) {
-		cerr << status.ToString() << endl;
-	} else {
-		cerr << db_name << " successfully opened/created!" << endl;
-	}
+		cout << status.ToString() << endl;
+	} 
+	// else {
+	// 	cerr << db_name << " successfully opened/created!" << endl;
+	// }
 }
 
 LevelDBHelper::~LevelDBHelper() {
@@ -33,21 +34,13 @@ int LevelDBHelper::get(const string& block_hash, string& data) {
 }
 
 int LevelDBHelper::put(const string& block_hash, const string& data) {
-    if(m_db == NULL) {
-        cout << "m_db was NULL!" << endl;
-        return 1;
-    } else {
-        cout << "m_db was NOT NULL!" << endl;
-	}
-
 	leveldb::Status status = m_db->Put(writeoptions, block_hash, data);
-//	leveldb::Status status = m_db->Write(writeoptions, &batch);
 
 	if (!status.ok()) {
 		cout << status.ToString() << endl;
 		return 1;
 	} 
-	cout << "(" << block_hash << "," << data << ") succesfully inserted!" << endl;
+	// cout << "(" << block_hash << "," << data << ") succesfully inserted!" << endl;
 	return 0;
 }
 
@@ -56,16 +49,3 @@ bool LevelDBHelper::alreadyExists(const string& block_hash) {
 	return (get(block_hash, dummy) == 0) ? true : false;
 }
 
-
-
-// int LevelDBHelper::connect(const std::string& dbName) {
-// 	leveldb::Options options;
-//     options.create_if_missing = true;
-//     leveldb::Status status = leveldb::DB::Open(options, dbName, &m_db);
-//     if (!status.ok()) {
-//     	cerr << status.ToString() << endl;
-//     	return 1;
-//     } 
-
-//     return 0;
-// }
