e31f09b822ae3f93c64618a7b2f2161c8a271ee4
stackedqueue <yang.julian@gmail.com>
2015-03-14 15:17:34 -0700

    match syntax arguments

--- blockcache/leveldb_helper.cc
+++ blockcache/leveldb_helper.cc
@@ -27,7 +27,7 @@ int LevelDBHelper::get(const string& block_hash, string& data) {
 
 //	string response_data((char*)response.data(), response.size());
     string response_data;
-    HttpHelper::sendLocalMsg(command, response_data, HttpHelper::leveldb_portno);
+    HttpHelper::sendLocalMsg(command, response_data, HttpHelper::leveldb_portno, true);
     if (response_data == "Operation failed")
 		return 1;
 	else 
@@ -45,7 +45,7 @@ int LevelDBHelper::put(const string& block_hash, const string& data) {
 //	m_socket.recv(&response);
 //	string response_data((char*)response.data(), response.size());
     string response_data;
-    HttpHelper::sendLocalMsg(command, response_data, HttpHelper::leveldb_portno);
+    HttpHelper::sendLocalMsg(command, response_data, HttpHelper::leveldb_portno, true);
     if (response_data == "Operation failed")
 		return 1;
 	else
